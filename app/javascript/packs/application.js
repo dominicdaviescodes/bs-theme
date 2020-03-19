3cc79b49ea0c098f448d585551bb1c9489f3d59c
dominicdaviescodes <codewithdom@gmail.com>
2020-03-19 22:48:30 +0000

    reconfigure bootstrap and add admin theme

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,12 +2,17 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-import 'bootstrap'
+
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import 'bootstrap/dist/js/bootstrap'
+import 'bootstrap/dist/css/bootstrap'
+
+import 'assets/javascripts/sb-admin-2.min.js'
+import 'assets/stylesheets/sb-admin-2.min.css'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
